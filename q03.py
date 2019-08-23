cae4b331a1cc69adda5fc5795af801ab6ca541a4
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 23:08:46 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,13 @@
 ## D,23
 ## E,67
 ##
+
+data=open('data.csv','r').readlines()
+data=[row.split('\t') for row in data]
+x=dict()
+for row in data: 
+  if row[0] not in x.keys():
+    x[row[0]]=0
+  x[row[0]]=x[row[0]]+int(row[1])
+for i in sorted(x.keys()):
+  print(i,',',x[i],sep='')
