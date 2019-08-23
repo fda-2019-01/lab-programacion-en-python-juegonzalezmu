d397223e159774291dbbaf55ccc950a4157b173a
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 23:19:04 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -18,7 +18,6 @@
 d = open('data.csv','r').readlines()
 d = [line[:-1] for line in d]
 d = [line.split('\t') for line in d]
-
 X = [line[2].split('-')[1] for line in d]
 a = set(X)
 for i in sorted(a):
