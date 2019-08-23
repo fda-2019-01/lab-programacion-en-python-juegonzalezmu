c5d0834fee689b326091da98126c6b6446efeb36
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 23:16:16 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -14,3 +14,12 @@
 ## 11,2
 ## 12,3
 ##
+
+d = open('data.csv','r').readlines()
+d = [line[:-1] for line in d]
+d = [line.split('\t') for line in d]
+
+X = [line[2].split('-')[1] for line in d]
+a = set(X)
+for i in sorted(a):
+    print(i+','+str(X.count(i)))
