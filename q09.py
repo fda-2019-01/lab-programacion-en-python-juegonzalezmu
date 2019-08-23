42b166073154f75f9151a4117fc37ca752f385c3
Juan Esteban Gonzalez Murcia <50414971+juegonzalezmu@users.noreply.github.com>
2019-08-22 23:39:03 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -17,3 +17,14 @@
 ## ('9', ['A', 'B', 'C', 'E'])
 ##
 ##
+
+data = open('data.csv','r').readlines()
+data = [line[:-1] for line in data]
+data = [line.split('\t') for line in data]
+list = []
+for key, group in itertools.groupby(sorted([[row[1], row[0]] for row in data]),itemgetter(0)):
+    x = sorted(set([line[1] for line in group]))
+    list.append((key,[value for value in x]))
+
+for row in list:
+    print(row)
